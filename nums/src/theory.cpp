2a8d763294c01cc43eda9983ce39b5fbb4c1f412
Nolan Miller <namiller@usc.edu>
2016-10-02 16:33:40 -0700

    implemented more

--- nums/src/theory.cpp
+++ nums/src/theory.cpp
@@ -2,17 +2,44 @@
 #include "primes.h"
 #include <vector>
 #include <utility>
+#include <cmath>
 
 using namespace std;
 
-//TODO: implement
 vector<int> continuing_fraction_sqrt(int s) {
-  return move(vector<int>());
+  int a = static_cast<int>(sqrt(s));
+  int m = 0;
+  int d = 1;
+  vector<int> ret = {a};
+  if (a*a == s) {
+    return ret;
+  }
+  while (a != *ret.begin() * 2) {
+    m = d*a - m;
+    d = (s - m*m)/d;
+    a = (*ret.begin() + m) / d;
+    ret.push_back(a);
+  }
+  return move(ret);
+}
+
+void gcd_update(int quotient, int *v, int *old_v) {
+  int tmp = *v;
+  *v = *old_v - quotient * *v;
+  *old_v = tmp;
 }
 
 //TODO: implement
 pair<int, int> extended_gcd(int a, int b) {
-  return move(make_pair(0, 0));
+  int vs[] = {0, 1, b};
+  int old_vs[] = {1, 0, a};
+  while (vs[2] != 0) {
+    int quotient = old_vs[2] / vs[2];
+    for (int i = 0; i++; i < 3) {
+      gcd_update(quotient, &(vs[i]), &(old_vs[i]));
+    }
+  }
+  return move(make_pair(vs[0], vs[1]));
 }
 
 //TODO: implement
@@ -22,7 +49,11 @@ int gcd(int a, int b) {
 
 //TODO: implement
 int totient(int n) {
-  return 0;
+  double r = n;
+  for (const auto & p : unique_prime_factors(n)) {
+    r *= 1.0 - 1.0 / p;
+  }
+  return static_cast<int>(r);
 }
 
 //TODO: implement
--- nums/test/theory_test.cpp
+++ nums/test/theory_test.cpp
@@ -1,8 +1,41 @@
 #include "../include/theory.h"
 #include <gtest/gtest.h>
 #include <vector>
+#include <utility>
 
 using namespace std;
+
+TEST(ContinuingSqrt, square) {
+  ASSERT_EQ(vector<int>({3}), continuing_fraction_sqrt(9));
+}
+TEST(ContinuingSqrt, unity) {
+  ASSERT_EQ(vector<int>({1}), continuing_fraction_sqrt(1));
+}
+TEST(ContinuingSqrt, zero) {
+  ASSERT_EQ(vector<int>({0}), continuing_fraction_sqrt(0));
+}
+TEST(ContinuingSqrt, repeating_two) {
+  ASSERT_EQ(vector<int>({1,2}), continuing_fraction_sqrt(2));
+}
+TEST(ContinuingSqrt, odd_length) {
+  ASSERT_EQ(vector<int>({2,2,4}), continuing_fraction_sqrt(6));
+}
+TEST(ContinuingSqrt, odd_long) {
+  ASSERT_EQ(vector<int>({8,1,1,1,16}), continuing_fraction_sqrt(75));
+}
+
+/*
+TEST(ExtendedGCD, no_repeat) {
+  ASSERT_EQ(make_pair(1,2), extended_gcd(5,2));
+}*/
+
+TEST(Totient, perfect) {
+  ASSERT_EQ(8, totient(24));
+}
+
+TEST(Totient, squart) {
+  ASSERT_EQ(3, totient(9));
+}
 
 TEST(ModExp, no_mod) {
   ASSERT_EQ(5*5*5*5, mod_exp(5,4,10000));
